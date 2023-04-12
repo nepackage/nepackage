769572d69ddad173c5c756a991a5786e3d98cafb
Oscar Tisnado <neptali.tisnado@outlook.com>
2023-04-12 15:00:58 -0600

    fix: remove required state key json in GitHubCredentialCreate model, by default set to false

--- models/githubModel.go
+++ models/githubModel.go
@@ -4,10 +4,10 @@ import "time"
 
 type GithubCredential struct {
 	ID        uint      `json:"id" gorm:"primary_key; not null; autoIncrement"`
-	Name      string    `json:"name" binding:"required" gorm:"not null"`
-	Username  string    `json:"username" binding:"required" gorm:"not null"`
-	Password  string    `json:"-" binding:"required" gorm:"not null"`
-	State     bool      `json:"state" binding:"required" gorm:"not null"`
+	Name      string    `json:"name" gorm:"not null"`
+	Username  string    `json:"username" gorm:"not null"`
+	Password  string    `json:"-" gorm:"not null"`
+	State     bool      `json:"state" gorm:"not null"`
 	CreatedAt time.Time `gorm:"not null"`
 	UpdatedAt time.Time `gorm:"not null"`
 }
@@ -17,17 +17,17 @@ type GithubCredentialCreate struct {
 	Name      string `json:"name" binding:"required"`
 	Username  string `json:"username" binding:"required"`
 	Password  string `json:"password" binding:"required"`
-	State     bool   `json:"state" binding:"required"`
+	State     bool   `json:"state"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
 type GithubCredentialUpdate struct {
-	ID        uint   `json:"id"`
-	Name      string `json:"name"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	State     bool   `json:"state"`
+	ID        uint   `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Password  string `json:"password,omitempty"`
+	State     bool   `json:"state,omitempty"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
